3b24c5a10be5294beea92d130268dbf2df489b09
alvaropenaleon <S3909789@student.rmit.edu.au>
2021-10-14 23:31:17 +1100

    Level 1
    Creates a route to Javascript

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -17,6 +17,7 @@ public class App {
 
     public static final int         JAVALIN_PORT    = 7000;
     public static final String      CSS_DIR         = "css/";
+    public static final String      JS_DIR          = "javascript/";
     public static final String      IMAGES_DIR      = "images/";
 
     public static void main(String[] args) {
@@ -26,6 +27,10 @@ public class App {
             
             // Uncomment this if you have files in the CSS Directory
             config.addStaticFiles(CSS_DIR);
+
+            // Javascript route
+            config.addStaticFiles(JS_DIR);
+
 
             // Uncomment this if you have files in the Images Directory
             config.addStaticFiles(IMAGES_DIR);
