f433e5617dd5268d7a3d7cc95c0727fb11c45c57
Scott Dunsdon <s3918676@student.rmit.edu.au>
2021-10-23 11:14:38 +1100

    page 5&6: commented out movies DB

--- src/main/java/app/Page6.java
+++ src/main/java/app/Page6.java
@@ -44,18 +44,18 @@ public class Page6 implements Handler {
 
         // Look up some information from JDBC
         // First we need to use your JDBCConnection class
-        JDBCConnection jdbc = new JDBCConnection();
+        //JDBCConnection jdbc = new JDBCConnection();
 
         // Next we will ask this *class* for the movies
-        ArrayList<String> movies = jdbc.getMovies();
+       //ArrayList<String> movies = jdbc.getMovies();
 
         // Add HTML for the movies list
         html = html + "<h1>Movies</h1>" + "<ul>";
 
         // Finally we can print out all of the movies
-        for (String movie : movies) {
-            html = html + "<li>" + movie + "</li>";
-        }
+        //for (String movie : movies) {
+        //    html = html + "<li>" + movie + "</li>";
+        //}
 
         // Finish the List HTML
         html = html + "</ul>";
