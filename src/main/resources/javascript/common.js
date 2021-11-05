967e2f49c7d778ecbb4b2fe12498ae96d19a04c9
alvaropenaleon <S3909789@student.rmit.edu.au>
2021-11-05 11:22:37 +1100

    Updates .Js

--- src/main/resources/javascript/common.js
+++ src/main/resources/javascript/common.js
@@ -1,5 +1,14 @@
 
 
+
+// Initialize DataTables
+
+$(document).ready( function () {
+    $('#table_id').DataTable();
+} );
+
+
+
 // *** HEADER HIDE AT SCROLLING JS (Currently not active)
 
 var prevScrollpos = window.pageYOffset;
@@ -17,48 +26,6 @@ window.onscroll = function() {
     prevScrollpos = currentScrollPos;
 }
 
-/*
-
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('header').outerHeight();
-
-$(window).scroll(function(event){
-    didScroll = true;
-});
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('header').removeClass('nav-down').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('header').removeClass('nav-up').addClass('nav-down');
-        }
-    }
-    
-    lastScrollTop = st;
-}
-
-// Credit: Gill @ codeseek.com */
 
 
 /// Mission page collapsible
@@ -78,12 +45,6 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
-
-// Initialize DataTables
-
-$(document).ready( function () {
-    $('#table_id').DataTable();
-} );
 
 
 
