381424830e89f6c86c960f4284376506a0c3d4a7
alvaropenaleon <S3909789@student.rmit.edu.au>
2021-11-07 19:46:48 +1100

    Updates neg values gap difference chart

--- src/main/java/app/Page5.java
+++ src/main/java/app/Page5.java
@@ -283,7 +283,13 @@ public class Page5 implements Handler {
                 html = html + "<td>" + String.format("%.1f", row.getPercentNonIndig()) + "</td>";
                 }
             }
-            html = html + "<td>" + String.format("%.1f", row.getGapScore()) + "</td>";
+            if (row.getGapScore() < 0) {
+                html = html + "<td><p id='green'>" + String.format("%.1f", row.getGapScore()) + "</p></td>";
+            }
+            else{
+                html = html + "<td><p id='red'>" + String.format("%.1f", row.getGapScore()) + "</p'></td>";
+            }
+            
             
 
             html = html + "</tr>";
@@ -475,5 +481,6 @@ public class Page5 implements Handler {
         checkbox = checkbox + "><label for='" + id + "'>" + label + "</label><span class='checkmark'></span></label><br>";
         return checkbox;
     }
+    
 
 }
